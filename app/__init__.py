80935567d4634cbb16117bf782d7143f9331dfc9
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2021-06-10 07:53:05 -0400

    added general debugging
    I did this  to avoid us going through environmental variables everytime. It's not a major change so it won't cause any conflict for now

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+# so we don't have to always go through environment variables
+if __name__ == '__main__':
+    app.run(debug=True)
