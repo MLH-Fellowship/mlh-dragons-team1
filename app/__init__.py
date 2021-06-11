34125576d716ff4e4db74eb450970f541c36cb84
PurpleUltralisk <uw.david.jiang@gmail.com>
2021-06-11 15:31:38 -0400

    add blog links

--- app/__init__.py
+++ app/__init__.py
@@ -25,15 +25,17 @@ def blog_page():
 
 @app.route('/showblog')
 def showblog():
-    id = request.args.get('id')
+    id = int(request.args.get('id'))
 
     dir = os.path.realpath(os.path.dirname(__file__))
     json_dir = os.path.join(dir, "static/data", "blogs.json")
     data = json.load(open(json_dir))
-    blogs=data['blogs']
+    blogs=data
     
-    blog = blogs[id]
-    return render_template('showblog.html', blog=blog)
+    blog = blogs["blogs"][id]
+    print(blog)
+    return  
+    # return render_template('showblog.html', blog=blog)
 
 @app.route('/projects')
 def project_page():
